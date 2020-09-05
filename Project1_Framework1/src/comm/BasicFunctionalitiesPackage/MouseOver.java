55a8931fce5197b20e508e72c40861fb733ce46f
helas <helas@192.168.0.16>
2020-09-05 15:10:45 +0200

    commit test

--- Project1_Framework1/src/comm/BasicFunctionalitiesPackage/MouseOver.java
+++ Project1_Framework1/src/comm/BasicFunctionalitiesPackage/MouseOver.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.interactions.Actions;
 
 public class MouseOver extends BasicOperations
 {
-	 public static WebElement MouseHover(WebElement value)		 
+	 public static WebElement MouseHover(WebElement value) throws InterruptedException		 
 	 {
 		Actions at = new Actions(driver);
 		at.moveToElement(value).perform();
